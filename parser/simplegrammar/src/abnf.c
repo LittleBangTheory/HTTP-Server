958da7b7f22d91cb015fc99c56c64a074657c14c
LittleBangTheory <gabicho05@gmail.com>
2023-03-02 15:01:36 +0100

    update abnf.c

--- parser/simplegrammar/src/abnf.c
+++ parser/simplegrammar/src/abnf.c
@@ -8,7 +8,8 @@
 */
 
 /**
- * \brief Start function
+ * \fn void start(int **current_char, node *struct_current)
+ * \brief Function to check the presence of 'start' in the beggining of the request
  * \param current_char Pointer of pointer to the current char of the request
  * \param struct_current Pointer to the current struct of the chained list. This parameter is initialized to NULL but allocated before the call of this function
 */
@@ -28,4 +29,40 @@ void start(int **current_char, node *struct_current){
         // Error
         exit(EXIT_FAILURE);
     }
+}
+
+
+/** \fn void mot(int **current_char, node *struct_current)
+ * \brief Function to check the composition of a word -> at least one char (a-z/A-Z) and a separator (SP, HTAB, -, _)
+ * \param current_char Pointer of pointer to the current char of the request
+ * \param struct_current Pointer to the current struct of the chained list. This parameter is initialized to NULL but allocated before the call of this function
+*/
+void mot(int **current_char, node *struct_current){
+    struct_current->s = current_char;
+    struct_current->taille = 0;
+    while(isalpha(**current_char)){
+        *current_char += 1;
+        struct_current->taille += 1;
+        //Implémenter une limite
+    }
+    if (**current_char == '%x20' || **current_char == '%x09' || **current_char == '%x2D' || **current_char == '%x5F'){
+        // Allocate and init the new struct (ptr, int...)
+        node* new_struct = malloc(sizeof(node));
+        new_struct->pere = struct_current;
+        struct_current->fils = new_struct;
+        *struct_current = *new_struct;
+        separateur(current_char, struct_current);
+    } else {
+        // Error
+        exit(EXIT_FAILURE);
+    }
+}
+
+void separateur(int **current_char, node *struct_current){
+    struct_current->fils = NULL;
+    struct_current->frere_g = NULL;
+    struct_current->frere_d = NULL;
+    struct_current->s = current_char;
+    struct_current->taille = 1;
+    *current_char += 1;
 }
