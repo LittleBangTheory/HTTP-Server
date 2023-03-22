0f36bdcf742c5d76007377b0dc547d6dfdd4d736
LittleBangTheory <gabicho05@gmail.com>
2023-03-22 22:16:22 +0100

    update abnf.c

--- parser/src/abnf.c
+++ parser/src/abnf.c
@@ -4,3 +4,35 @@
 #include <ctype.h>
 #include "../headers/abnf.h"
 
+//start line = request line
+void start_line(char **current_char, node *struct_current){
+    // Init the struct (ptr, int...)
+    struct_current->s = *current_char;
+    //TODO : add label start_line to struct_current->label 
+    struct_current->fils = NULL;
+    struct_current->frere = NULL;
+
+    // Allocate memory for the first child
+    node *new_struct = malloc(sizeof(node));
+    struct_current->fils = new_struct;
+
+    // Call the function for the first child, supposed to be method
+    method(current_char, new_struct);
+    //TODO : struct_current->taille à attribuer au return de la fonction fils pour savoir ou la start line s'arrête
+}
+
+//method = token
+void method(char **current_char, node *struct_current){
+    // Init the struct (ptr, int...)
+    struct_current->s = *current_char;
+    //TODO : add label method to struct_current->label 
+    struct_current->fils = NULL;
+    struct_current->frere = NULL;
+
+    // Allocate memory for the first child
+    node *new_struct = malloc(sizeof(node));
+    struct_current->fils = new_struct;
+
+    // TODO : Call tchar to store 
+    //TODO : struct_current->taille à attribuer au return de la fonction fils pour savoir ou la methode s'arrête
+}
