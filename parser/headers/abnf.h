67357e944d5131c94ba95273e50e5d53bd38ad88
LittleBangTheory <gabicho05@gmail.com>
2023-03-23 13:34:30 +0100

    update abnf

--- parser/headers/abnf.h
+++ parser/headers/abnf.h
@@ -1,8 +1,12 @@
 typedef struct abnf node;
 struct abnf {
    char *label; //a def dans le main
-   char *s;
-   unsigned int taille;
+   char *debut;
+   char *fin;
    node *fils;
    node *frere;
 };
+
+#define START_LINE 0
+#define METHOD 1
+#define TCHAR 2
--- parser/src/abnf.c
+++ parser/src/abnf.c
@@ -4,11 +4,13 @@
 #include <ctype.h>
 #include "../headers/abnf.h"
 
+char tchar_list[] = {'0x21', '0x23', '0x24', '0x25', '0x26', '0x27', '0x2a', '0x2b', '0x2d', '0x2e', '0x5e', '0x5f', '0x60', '0x7c', '0x7e'};
+
 //start line = request line
-void start_line(char **current_char, node *struct_current){
+void start_line(char **current_char, node *struct_current, char *label){
     // Init the struct (ptr, int...)
-    struct_current->s = *current_char;
-    //TODO : add label start_line to struct_current->label 
+    struct_current->debut = *current_char;
+    struct_current->label = label+START_LINE;
     struct_current->fils = NULL;
     struct_current->frere = NULL;
 
@@ -17,15 +19,19 @@ void start_line(char **current_char, node *struct_current){
     struct_current->fils = new_struct;
 
     // Call the function for the first child, supposed to be method
-    method(current_char, new_struct);
-    //TODO : struct_current->taille à attribuer au return de la fonction fils pour savoir ou la start line s'arrête
+    method(current_char, new_struct, label);
+
+    // The end is known when the method() is done
+    struct_current->fin = *current_char;
+
+    // TODO : call the brother
 }
 
 //method = token
-void method(char **current_char, node *struct_current){
+void method(char **current_char, node *struct_current, char *label){
     // Init the struct (ptr, int...)
-    struct_current->s = *current_char;
-    //TODO : add label method to struct_current->label 
+    struct_current->debut = *current_char;
+    struct_current->label = label+METHOD;
     struct_current->fils = NULL;
     struct_current->frere = NULL;
 
@@ -33,6 +39,44 @@ void method(char **current_char, node *struct_current){
     node *new_struct = malloc(sizeof(node));
     struct_current->fils = new_struct;
 
-    // TODO : Call tchar to store 
-    //TODO : struct_current->taille à attribuer au return de la fonction fils pour savoir ou la methode s'arrête
+    tchar(current_char, new_struct, label);
+    struct_current->fin = *current_char;
+}
+
+void tchar(char **current_char, node *struct_current, char *label){
+    // Init the struct (ptr, int...)
+    struct_current->debut = *current_char;
+    struct_current->label = label+TCHAR;
+    struct_current->fils = NULL;
+    struct_current->frere = NULL;
+
+    // Allocate memory for the first child
+    node *new_struct = malloc(sizeof(node));
+    struct_current->fils = new_struct;
+
+    if(isalpha(**current_char)){
+        // Allocate memory for the first child
+        node *new_struct = malloc(sizeof(node));
+        struct_current->fils = new_struct;
+
+        alpha(current_char, new_struct, label);
+    } else if(isdigit(**current_char)){
+        // Allocate memory for the first child
+        node *new_struct = malloc(sizeof(node));
+        struct_current->fils = new_struct;
+
+        digit(current_char, new_struct, label);
+    } else if(istchar(**current_char) == 1){
+        
+    }
+
+}
+
+int istchar(char c){
+    for(int i=0; i<15; i++){
+        if(c == tchar_list[i]){
+            return 1;
+        }
+    }
+    return 0;
 }
