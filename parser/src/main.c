653717c50d652b4c08430dd1ce60ff0c8b991980
Spectraal <julienharrow@gmail.com>
2023-03-23 08:35:31 -0400

    main.c edited

--- parser/src/main.c
+++ parser/src/main.c
@@ -1,64 +1,20 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h> 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <errno.h>
 
-#include "../headers/api.h" 
+#include "../headers/api.h"
+#include "../headers/abnf.h"
 
-#define false 0 
+#define true 1
+#define false 0
 
-int main(int argc,char *argv[])
+int main(int argc, char const *argv[])
 {
-	int res,i,fi;
-	char *p=NULL,*addr;
+	if (argc!=2){printf("Usage : ./parsername <file>\n");return false;}
+	char* label[]={"start-line","CRLF","pchar","tchar","ALPHA","DIGIT"};
+	FILE* fic = NULL;
+	fic=fopen(argv[1],"r");
+	if(fic==NULL){printf("Erreur ouverture\n");return false;}
 
-	
-        struct stat st;
 
-	if (argc < 2 ) { printf("Usage: httpparser <file> <search>\nAttention <search> is case sensitive\n");  return 0; }
-	/* ouverture du fichier contenant la requête */ 
-	if ((fi=open(argv[1],O_RDWR)) == -1) {
-                perror("open");
-                return false;
-        }
-        if (fstat(fi, &st) == -1)           /* To obtain file size */
-                return false;
-        if ((addr=mmap(NULL,st.st_size,PROT_WRITE,MAP_PRIVATE, fi, 0)) == NULL )
-                return false;
-
-	// This is a special HACK since identificateur in C can't have character '-'
-
-	if (argc == 3 ) { 
-		p=argv[2]; 	
-		printf("searching for %s\n",p); 
-		while (*p) { 
-			if (*p=='-') { *p='_'; }
-			p++; 
-		}
-		p=argv[2]; 	
-	}
-	// call parser and get results. 
-	if (res=parseur(addr,st.st_size)) {
-		_Token *r,*tok; 
-		void *root=NULL;
-		root=getRootTree(); 
-		r=searchTree(root,p); 
-		tok=r; 
-		while (tok) {
-			int l; 
-			char *s; 
-			s=getElementValue(tok->node,&l); 
-			printf("FOUND [%.*s]\n",l,s);
-			tok=tok->next; 
-		}
-		purgeElement(&r);
-		purgeTree(root);
-	}
-	close(fi);
-	return(res); 
 }
