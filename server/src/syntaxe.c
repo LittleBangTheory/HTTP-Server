390476008e13d5934bbdc5cab7a75ed4c36651a3
LittleBangTheory <gabicho05@gmail.com>
2023-05-10 12:41:19 +0200

    update syntaxe.c

--- server/src/syntaxe.c
+++ server/src/syntaxe.c
@@ -109,34 +109,6 @@ int existing(char* s,int longueur, char* path, int pathLen){
 	return res;
 }
 
-/*Algorithm :
-   2.  While the input buffer is not empty, loop as follows:
-
-       A.  If the input buffer begins with a prefix of "../" or "./",
-           then remove that prefix from the input buffer; otherwise,
-
-       B.  if the input buffer begins with a prefix of "/./" or "/.",
-           where "." is a complete path segment, then replace that
-           prefix with "/" in the input buffer; otherwise,
-
-       C.  if the input buffer begins with a prefix of "/../" or "/..",
-           where ".." is a complete path segment, then replace that
-           prefix with "/" in the input buffer and remove the last
-           segment and its preceding "/" (if any) from the output
-           buffer; otherwise,
-
-       D.  if the input buffer consists only of "." or "..", then remove
-           that from the input buffer; otherwise,
-
-       E.  move the first path segment in the input buffer to the end of
-           the output buffer, including the initial "/" character (if
-           any) and any subsequent characters up to, but not including,
-           the next "/" character or the end of the input buffer.
-
-   3.  Finally, the output buffer is returned as the result of
-       remove_dot_segments.
-*/
-
 /**
  * @brief remove the dot segments from the path
  * 
@@ -394,6 +366,3 @@ int analyze(char* request,int clientID){
 	
 	return returnValue;
 }
-
-
-
